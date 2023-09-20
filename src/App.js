a33ca4b3e52719ad07c31f15c97a564b288bef22
Ian Fay <ianfay2003@gmail.com>
2023-09-20 09:30:51 -0500

    API Update

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import axios from 'axios';
 import './App.css';
 
 const App = () => {
-  const apiEndpoint = "https://medalsapi.azurewebsites.net/api/country";
+  const apiEndpoint = "https://medals-api-6.azurewebsites.net/api/country";
   const [ countries, setCountries ] = useState([]);
   const medals = useRef([
     { id: 1, name: 'gold' },
